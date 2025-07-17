c8f5a13b4b5b06e671cc7a82be9edef874500181
yehortpk <e2000work@gmail.com>
2025-07-17 04:15:29 -0700

    feat: add extract_same_host_links and extract_hostname methods to parser

--- parser.py
+++ parser.py
@@ -3,40 +3,52 @@ from urllib.robotparser import RobotFileParser
 
 from lxml import html
 
+
 class HTMLParser:
-    def __init__(self, content: str, hostname: str = None):
+    def __init__(self, content: str):
         self.content = content
         self.tree = html.fromstring(self.content)
 
+    def extract_links(self, hostname: str = None) -> dict[str, list[str]]:
         if hostname:
-            parsed_url = urlparse(hostname)
-            if parsed_url.scheme:
-                self.hostname = parsed_url.scheme + parsed_url.path
-            self.hostname = "https://" + parsed_url.path
-
-
-    def extract_links(self) -> dict[str, list[str]]:
-        if self.hostname:
-            self.tree.make_links_absolute(self.hostname)
+            hostname = self._extract_hostname(hostname)
+            self.tree.make_links_absolute(hostname)
 
         links = {}
 
         for element, attribute, link, pos in self.tree.iterlinks():
             if element.tag == 'a' and attribute == "href":
-                link_hostname, url = urlparse(link).hostname, urldefrag(link).url
-                if link_hostname is None and self.hostname:
-                    link_hostname = self.hostname
+                link_hostname, url = self._extract_hostname(link), urldefrag(link).url
+                if link_hostname is None and hostname:
+                    link_hostname = hostname
 
                 host_links: list[str] = links.get(link_hostname, [])
                 host_links.append(url)
                 links[link_hostname] = host_links
 
-
         return links
+
+    def extract_same_host_links(self, hostname: str):
+        hostname = self._extract_hostname(hostname)
+        return self.extract_links(hostname).get(hostname, [])
+
+    def _extract_hostname(self, url):
+        parsed_url = urlparse(url)
+
+        if not parsed_url.netloc:
+            return None
+
+        if parsed_url.scheme:
+            hostname = parsed_url.scheme + "://" + parsed_url.netloc
+        else:
+            hostname = "https://" + parsed_url.netloc
+
+        return hostname
+
 
 class RobotsParser:
     def __init__(self, host: str):
-        self.robots_parser =  RobotFileParser()
+        self.robots_parser = RobotFileParser()
         self.robots_parser.set_url(f"{host}/robots.txt")
         self.robots_parser.read()
 
