716c3efc7a97812e28488ceebd5c3a4ffac0c8a0
yehortpk <e2000work@gmail.com>
2025-07-18 07:11:42 -0700

    feat: move error handling from scrapper to runner

--- runner.py
+++ runner.py
@@ -1,11 +1,54 @@
+import asyncio
+import logging
+
+from aiohttp import ClientResponseError, ClientError
+from lxml.etree import ParserError
+
 from parser import HTMLParser
 from scrapper import StaticScrapper
+
+logger = logging.getLogger(__name__)
 
 
 class ScrapperRunner:
     @staticmethod
     async def handle_request(url: str) -> dict[str, list[str]]:
-        res = await StaticScrapper(url).fetch()
-        parser = HTMLParser(res.html)
-        links = parser.extract_same_host_links(url)
-        return links
+        try:
+            res = await StaticScrapper(url).fetch()
+        except ClientResponseError as e:
+            logger.error(f"HTTP error {e.status} for URL {url}: {e.message}")
+            # Handle or log specific HTTP error codes
+            return {}
+
+        except ClientError as e:
+            # This covers connection errors, timeouts, etc.
+            logger.error(f"Client error for URL {url}: {e}")
+
+            return {}
+
+        except asyncio.TimeoutError:
+            logger.error(f"Timeout error for URL {url}")
+
+            return {}
+
+        except Exception as e:
+            # Catch-all for unexpected exceptions
+            logger.error(f"Unexpected error for URL {url}: {e}")
+
+            return {}
+
+        try:
+            parser = HTMLParser(res.html)
+            links = parser.extract_same_domain_links(url)
+            return links
+        except ParserError as e:
+            logger.error(f"{url} {e}")
+            return {}
+
+        except Exception as e:
+            # Catch-all for unexpected exceptions
+            logger.error(f"Unexpected error for URL {url}: {e}")
+
+            return {}
+
+
--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,7 @@
-import asyncio
 from urllib.parse import urlparse
 
 import aiohttp
-from aiohttp import ClientTimeout, ClientResponseError, ClientError
+from aiohttp import ClientTimeout
 
 from config import ScrapperConfig
 from models import ScrappingResult
@@ -16,56 +15,17 @@ class StaticScrapper:
     async def fetch(self) -> ScrappingResult:
         config: ScrapperConfig = ScrapperConfig(self.url)
 
-        try:
-            async with aiohttp.ClientSession(
-                    headers=config.headers,
-                    cookies=config.cookies,
-                    timeout=ClientTimeout(total=config.timeout_ms/1000)
-            ) as session:
-                async with session.get(self.url, proxy=config.proxy) as response:
-                    html = await response.text()
-                    result = ScrappingResult(
-                        url=str(response.url),
-                        status=response.status,
-                        html=html
-                    )
+        async with aiohttp.ClientSession(
+                headers=config.headers,
+                cookies=config.cookies,
+                timeout=ClientTimeout(total=config.timeout_ms/1000)
+        ) as session:
+            async with session.get(self.url, proxy=config.proxy) as response:
+                html = await response.text()
+                result = ScrappingResult(
+                    url=str(response.url),
+                    status=response.status,
+                    html=html
+                )
 
-                    return result
-
-        except ClientResponseError as e:
-            print(f"HTTP error {e.status} for URL {self.url}: {e.message}")
-            # Handle or log specific HTTP error codes
-            return ScrappingResult(
-                        url=str(response.url),
-                        status=e.status,
-                        html=""
-                    )
-
-        except ClientError as e:
-            # This covers connection errors, timeouts, etc.
-            print(f"Client error for URL {self.url}: {e}")
-
-            return ScrappingResult(
-                url=str(response.url),
-                status=400,
-                html=""
-            )
-
-        except asyncio.TimeoutError as e:
-            print(f"Timeout error for URL {self.url}")
-
-            return ScrappingResult(
-                url=str(response.url),
-                status=408,
-                html=""
-            )
-
-        except Exception as e:
-            # Catch-all for unexpected exceptions
-            print(f"Unexpected error for URL {self.url}: {e}")
-
-            return ScrappingResult(
-                url=str(response.url),
-                status=500,
-                html=""
-            )
+                return result
